c5c16429c638b83b2940e61fe0b72a70959bf529
Priyanshu Chourasia <priyanshuchourasia916@gmail.com>
2025-05-05 13:07:42 +0530

    new information added

--- src/main/java/com/wishalpha/priyanshu/TaskManagementSystem/controllers/IndexController.java
+++ src/main/java/com/wishalpha/priyanshu/TaskManagementSystem/controllers/IndexController.java
@@ -1,4 +1,16 @@
 package com.wishalpha.priyanshu.TaskManagementSystem.controllers;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
 public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+        return "Welcome to API Example";
+    }
 }
--- src/main/java/com/wishalpha/priyanshu/TaskManagementSystem/entities/User.java
+++ src/main/java/com/wishalpha/priyanshu/TaskManagementSystem/entities/User.java
@@ -1,4 +1,28 @@
 package com.wishalpha.priyanshu.TaskManagementSystem.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "username", unique = true)
+    private String username;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "phone_number")
+    private Long phoneNumber;
+
 }
