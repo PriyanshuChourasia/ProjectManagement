a0af9a50b0e433295400d3b23e9fdfbc8f09b77e
Priyanshu Chourasia <priyanshuchourasia916@gmail.com>
2025-05-05 13:05:27 +0530

    files delete to create api

--- src/main/java/com/wishalpha/priyanshu/TaskManagementSystem/controllers/IndexController.java
+++ src/main/java/com/wishalpha/priyanshu/TaskManagementSystem/controllers/IndexController.java
@@ -1,15 +1,4 @@
 package com.wishalpha.priyanshu.TaskManagementSystem.controllers;
 
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
 public class IndexController {
-
-    @GetMapping("/")
-    public String index(HttpServletRequest request)
-    {
-        return "Welcome To Spring Boot java ," + request.getSession().getId();
-    }
 }
--- src/main/java/com/wishalpha/priyanshu/TaskManagementSystem/entities/User.java
+++ src/main/java/com/wishalpha/priyanshu/TaskManagementSystem/entities/User.java
@@ -1,42 +1,4 @@
 package com.wishalpha.priyanshu.TaskManagementSystem.entities;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
-import java.time.Instant;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Entity
-@Table(name = "users")
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_gen")
-    @SequenceGenerator(name = "user_gen", sequenceName = "user_seq", allocationSize = 1)
-    private Integer id;
-
-    @Column(nullable = false)
-    private String name;
-
-//    @Column(nullable = false, unique = true)
-//    private String email;
-
-    @Column(name = "username")
-    private String username;
-
-    @Column
-    private  String password;
-
-//    @Column(name="created_at")
-//    @CreationTimestamp
-//    private Instant createdAt;
-//
-//    @Column(name="updated_at")
-//    @UpdateTimestamp
-//    private Instant updatedAt;
 }
